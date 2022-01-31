efd3b9df80a55f25ab1f74a606fe4591af7b5d4b
jagal <jagal@Pongso-SDS-Notebook>
2022-01-31 11:40:42 +0900

    2022-01-31

--- src/시험출제문제/시험_220129_통행료.java
+++ src/시험출제문제/시험_220129_통행료.java
@@ -51,23 +51,25 @@ import java.util.*;
 #3 32
 */
 public class 시험_220129_통행료 {
-	static int N, Q;
-	static int LGN, MAX;
+	static int T, N, Q;
+	static int LGN;
 	static int[][] parent, arrMax;
 	static int[] depth;
 	static long[] dist;
-	static long result;
-	static ArrayList<Node>[] arrList;
 
 	static class Node {
 		int dest;
 		int cost;
 
-		public Node(int dest, int cost) {
+		Node(int dest, int cost) {
 			this.dest = dest;
 			this.cost = cost;
 		}
 	}
+
+	static ArrayList<Node>[] arrList;
+	static long result;
+	static int MAX;
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("input_data.txt"));
@@ -75,20 +77,18 @@ public class 시험_220129_통행료 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(br.readLine().trim());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine().trim());
 			N = Integer.parseInt(st.nextToken());
 			Q = Integer.parseInt(st.nextToken());
-
 			arrList = new ArrayList[N + 1];
-			for (int i = 0; i <= N; i++) {
+			for (int i = 0; i <= N; i++)
 				arrList[i] = new ArrayList<>();
-			}
-
+			
 			// 인접리스트
 			for (int i = 1; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
+				st = new StringTokenizer(br.readLine().trim());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int c = Integer.parseInt(st.nextToken());
@@ -96,63 +96,51 @@ public class 시험_220129_통행료 {
 				arrList[b].add(new Node(a, c));
 			}
 
-			//
 			int nodeCnt = 1;
 			LGN = 0;
 			while (nodeCnt < N) {
 				nodeCnt *= 2;
 				LGN++;
 			}
+			
 			parent = new int[LGN + 1][N + 1];
 			arrMax = new int[LGN + 1][N + 1];
 			depth = new int[N + 1];
+			
 			Arrays.fill(depth, -1);
 			dist = new long[N + 1];
-
+			bfs(1);
 			findAncestor();
-			
-			// 질의 Q
+
 			result = 0;
 			for (int i = 0; i < Q; i++) {
 				st = new StringTokenizer(br.readLine().trim());
-	            int a = Integer.parseInt(st.nextToken());
-	            int b = Integer.parseInt(st.nextToken());
-	            int lca = LCA(a, b);
-	            result += dist[a] + dist[b] - 2*dist[lca] - MAX; 
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int lca = LCA(a, b);
+				result += (dist[a] + dist[b] - 2 * dist[lca] - MAX);
 			}
-			bw.write("#" + tc + result + "\n");
+			bw.write("#" + test_case + " " + result + "\n");
 			bw.flush();
-
-		} // text case
-
-	} // main
-
-	static int LCA(int x, int y) {
-		MAX = Integer.MIN_VALUE;
-		if(depth[x] > depth[y]) {
-			int tmp = x;
-			x = y;
-			y=tmp;			
 		}
-		
-		for (int i = LGN; i >= 0; i--) {
-			if(depth[y]-depth[x] >= (1<<i)) {
-				y = parent[i][y];
+	}
+
+	static void bfs(int n) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(n);
+		depth[n] = 0;
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			for (Node next : arrList[now]) {
+				if (depth[next.dest] != -1)
+					continue;
+				depth[next.dest] = depth[now] + 1;
+				parent[0][next.dest] = now;
+				dist[next.dest] = dist[now] + next.cost;
+				arrMax[0][next.dest] = next.cost;
+				q.add(next.dest);
 			}
 		}
-		
-		if(x==y) return x;
-		
-		for(int i=LGN; i>=0; i--) {
-			if(parent[i][x] != parent[i][y]) {
-				MAX = Math.max(MAX, Math.max(arrMax[i][x], arrMax[i][y]));
-				x = parent[i][x];
-				y = parent[i][y];
-			}
-		}
-		
-		MAX = Math.max(MAX, Math.max(arrMax[0][x], arrMax[0][y]));
-		return parent[0][x];
 	}
 
 	static void findAncestor() {
@@ -164,4 +152,31 @@ public class 시험_220129_통행료 {
 		}
 	}
 
+	static int LCA(int x, int y) {
+		MAX = Integer.MIN_VALUE;
+		if (depth[x] > depth[y]) {
+			int tmp = x;
+			x = y;
+			y = tmp;
+		}
+
+		for (int i = LGN; i >= 0; i--)
+			if (depth[y] - depth[x] >= (1 << i)) {
+				MAX = Math.max(MAX, arrMax[i][y]);
+				y = parent[i][y];
+			}
+
+		if (x == y)
+			return x;
+
+		for (int i = LGN; i >= 0; i--)
+			if (parent[i][x] != parent[i][y]) {
+				MAX = Math.max(MAX, Math.max(arrMax[i][x], arrMax[i][y]));
+				x = parent[i][x];
+				y = parent[i][y];
+			}
+
+		MAX = Math.max(MAX, Math.max(arrMax[0][x], arrMax[0][y]));
+		return parent[0][x];
+	}
 }
